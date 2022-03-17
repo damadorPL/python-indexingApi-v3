87d7a70749e22973a632b75a6c7c8253ed909d91
rolandadamczyk <rolandadamczyk@gmail.com>
2022-03-17 13:19:38 +0100

    Add exception ToMuchURLS

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 from oauth2client.service_account import ServiceAccountCredentials
 import httplib2
 import json
+
+class ToMuchURLS:
+    """Wyjątek gdy adresów jest więcej niż 100"""
+
+
 '''
 url = "url do zaindeksowania"
 '''
-print("Podaj adres do zaindeksowania")
+print("Podaj adres do zaindeksowania (max 100), odzielone przecinkiem.")
 url = str(input())
 split = ","
 if split in url:
     urls = url.split(", ")
 else:
     urls = url.split(",")
+"""
+Poniższe ograniczenie można zwiększyć, jeśli został zwiększony limit w Indexing Api w GCP.
+"""
+if len(urls) > 100:
+    raise ToMuchURLS('Za dużo adresów URL, maksymalnie 100')
 '''
 JSON_KEY_FILE = "scieżka do pliku z kluczem api"
 '''
@@ -30,10 +40,12 @@ URL_DELETED - usunięcie urla z indeksu
 Wystarczy zmienić poniższy 'type'
 '''
 for url in urls:
-    content = {}
-    content['url'] = url
-    content['type'] = "URL_UPDATED"
-    json_content = json.dumps(content)
-    print(content)
-    response, content = http.request(ENDPOINT, method="POST", body=json_content)
-    result = json.loads(content.decode())
+    send_url = {}
+    send_url['url'] = url
+    send_url['type'] = "URL_UPDATED"
+    json_content = json.dumps(send_url)
+    print(send_url)
+    response, send_url = http.request(ENDPOINT,
+                                      method="POST",
+                                      body=json_content)
+    result = json.loads(send_url.decode())
